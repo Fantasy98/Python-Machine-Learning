f6a5664312f3d5546747c3356c5338ec837a31c9
Fantasy98 <1563143127@qq.com>
2022-07-19 10:35:18 +0200

    Chapter7 Finshed

--- Deep_Learning_With_python/TEST.py
+++ Deep_Learning_With_python/TEST.py
@@ -1,3 +1,9 @@
 import tensorflow as tf
 
+
+##Find your .dll file's path and add them into environment 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+##
+### pip install graphviz 
+### pip install pydot_ng
