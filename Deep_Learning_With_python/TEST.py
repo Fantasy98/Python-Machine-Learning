130a13846eb45f8850a21630d66cc41ce88b6ca3
Fantasy98 <1563143127@qq.com>
2022-07-19 20:50:03 +0200

    Chapter 8 Finished

--- Deep_Learning_With_python/TEST.py
+++ Deep_Learning_With_python/TEST.py
@@ -1,5 +1,8 @@
-from tabnanny import verbose
+
+
 import tensorflow as tf
+import torch
+
 
 
 ##Find your .dll file's path and add them into environment 
@@ -9,3 +12,8 @@ print(tf.test.gpu_device_name())
 ##
 ### pip install graphviz 
 ### pip install pydot_ng
+
+from torch import nn
+
+points = torch.tensor([[4.0,1.0]]).to(device='cuda')
+print(points) 
