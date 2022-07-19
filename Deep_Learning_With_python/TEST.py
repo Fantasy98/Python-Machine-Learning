1b8a59dbbffb7198a5e58dcdcd6af35043c59332
Fantasy98 <1563143127@qq.com>
2022-07-19 11:19:23 +0200

    Morning 7.19

--- Deep_Learning_With_python/TEST.py
+++ Deep_Learning_With_python/TEST.py
@@ -1,9 +1,11 @@
+from tabnanny import verbose
 import tensorflow as tf
 
 
 ##Find your .dll file's path and add them into environment 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+print(tf.test.gpu_device_name())
 ##
 ### pip install graphviz 
 ### pip install pydot_ng
